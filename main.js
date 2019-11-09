41a4f46b591fc68cd6dba7661b871e044282c493
mahendra <mahendra@arraigntech.com>
2019-11-10 00:41:50 +0530

    initial files added

---
+++ main.js
@@ -0,0 +1,5 @@
+const fontAwesome = document.createElement("script");
+// <!-- Place your kit's code here -->
+fontAwesome.src = "https://kit.fontawesome.com/Your Kits.js";
+fontAwesome.crossOrigin = "anonymous";
+document.head.appendChild(fontAwesome);
